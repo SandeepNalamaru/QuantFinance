441632cd9c179fae7835ff2b2ad08afd827b5d33
Sandeep Nalamaru <62670816+SandeepNalamaru@users.noreply.github.com>
2024-11-12 03:27:55 -0500

    Add files via upload

--- Jai.py
+++ Jai.py
@@ -1,28 +1,34 @@
 import pandas as pd
 import streamlit as st
 
-st.title("Why Jai should/shouldn't move out")
-st.subheader('~ Fuck you Jai')
+st.title("Why you should/shouldn't move out in Jan")
 st.subheader('Use sidebar to change inputs <<<')
 
 with st.sidebar:
-    util = st.number_input('Average Utilites Cost', value=50)
-    travel = st.slider('Number of commutes to college per week',value = 4,max_value=7 )
-    months = st.slider('Number of sublet months', value = 3,max_value=7)
-    sub_rent = st.number_input('Sublet Rent in 858', value = 700)
+    rent_858 = st.number_input('Current House Rent', value = 800)
+    current_util = st.number_input('Current utilites Cost', value=50)
+    travel_cost = st.number_input('Current Travel Cost per week', value=20)
+
     
-if util and travel and months and sub_rent:
-    travel_cost = (2.4*travel)+(2.4*4*4)
-    rent_858 = 800
-    rent_new = 500
-    cost_sublet = (rent_858 - sub_rent)*months
-    rent_new_total = rent_new + travel_cost + util
-    rent_858_new = rent_858 + (2.4*4*4)
+    rent_new = st.number_input('New House Rent', value = 800)
+    util = st.number_input('New House Utilites Cost', value=50)
+    new_travel_cost = st.number_input('New Travel Cost per week', value=20)
+    
+    
+    months = st.slider('Number of sublet months', value = 3,max_value=7)
+    
+    
+if util:
+    
+
     living_months = 8 - months
     sublet_months = int(months)
-    data = {'Rent Type': ['858', 'New House'], 'Cost': [rent_858_new, rent_new_total]}
+    
+    rent_new_total = rent_new + new_travel_cost*4 + util
+    rent_858_new = rent_858 + current_util + travel_cost*4
+    
+    data = {'Rent Type': ['Current House', 'New House'], 'Cost': [rent_858_new, rent_new_total]}
 
-    # Create a DataFrame
     df = pd.DataFrame(data).set_index('Rent Type')
 
     st.write('Monthly Cost (rent,utilities and transport): ')
@@ -30,35 +36,49 @@ if util and travel and months and sub_rent:
     
     
     months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug']
-    rent_858_months = [rent_858_new]*living_months
-    rent_858_sublet = [rent_858-sub_rent]*sublet_months
-    rent_858_total = rent_858_months + rent_858_sublet
     
-    rent_new_living_months = [rent_new_total]*living_months
     
-    agree = st.checkbox("He gets a sublet in New House")
     
-    if agree:
-        rent_new_sublet_months = [0]*sublet_months
+    
+
+    old_house_sublet_rent =0   
+    old_house_agree = st.checkbox("You get a sublet in Current House")
+    
+    if old_house_agree:
         
+        old_house_sublet_rent = st.number_input('Sublet Rent for Current House: ', value=rent_858)
+        rent_858_sublet_months = [rent_858 - old_house_sublet_rent]*sublet_months
+    else:
+        rent_858_sublet_months = [rent_858]*sublet_months
+        
+    rent_858_sublet = [rent_858-old_house_sublet_rent]*sublet_months
+    rent_858_months = [rent_858_new]*living_months
+    rent_858_total = rent_858_months + rent_858_sublet_months
+        
+    new_house_agree = st.checkbox("You get a sublet in New House")
+    
+    if new_house_agree:
+        new_house_sublet_rent = st.number_input('Rent charged for sublet', value=rent_new)
+        rent_new_sublet_months = [rent_new-new_house_sublet_rent]*sublet_months
     else:
         rent_new_sublet_months = [rent_new]*sublet_months
-        
-    rent_new_final = rent_new_living_months + rent_new_sublet_months
     
+    rent_new_living_months = [rent_new_total]*living_months 
+    rent_new_final = rent_new_living_months + rent_new_sublet_months
+
     
     data_1 = {
     'Months': months,
-    '858': rent_858_total,  # Use the generated list for rent_858_new
+    'Current House': rent_858_total,  # Use the generated list for rent_858_new
     'New House': rent_new_final  # Same rent for New House across 3 months
     }
     
     df1 = pd.DataFrame(data_1)
     
-    df1['Cumulative_858'] = df1['858'].cumsum()
-    df1['Cumulative_New_house'] =  df1['New House'].cumsum()
+    df1['Current_House'] = df1['Current House'].cumsum()
+    df1['New_house'] =  df1['New House'].cumsum()
 
-    df1.pop('858')
+    df1.pop('Current House')
     df1.pop('New House')
     
     df1['Date'] = pd.to_datetime(df1['Months'] + ' 2024', format='%b %Y')
@@ -66,17 +86,30 @@ if util and travel and months and sub_rent:
     df1.pop('Months')
     
     st.line_chart(df1)
+    st.dataframe(df1)
     
-    Cum_858_rent = df1.iloc[7, df1.columns.get_loc('Cumulative_858')]
-    Cum_new_rent = df1.iloc[7, df1.columns.get_loc('Cumulative_New_house')]
-    Diff = round(Cum_858_rent - Cum_new_rent,2)
-    conclusion = f"the difference in rent between 858 and New House: ${Diff}"
-    st.subheader(conclusion)
+    Cum_858_rent = df1.iloc[7, df1.columns.get_loc('Current_House')]
+    Cum_new_rent = df1.iloc[7, df1.columns.get_loc('New_house')]
+    text = f"Cost of Current House: {Cum_858_rent}  |  Cost of New House: {Cum_new_rent}"
+    st.write(text)
+    Diff = Cum_858_rent - Cum_new_rent
     
-    move_in = st.number_input('Potential Move-in costs', value = 15)
-    diff1 = round(Diff-move_in,2)
-    conclusion1 = f"Jai's savings: ${diff1}"
+    move_in = st.number_input('Potential Move-in costs', value = 50)
+    diff1 = Diff-move_in
+    conclusion1 = f"the difference in rent between Old and New House: ${diff1}"
     st.header(conclusion1)
+    
+    more = st.checkbox("Want more?")
+    if more:
+        food = st.number_input('Enter Monthly Food costs',value=100)
+        inflow = st.number_input('Monthly Budget', value = 1200)
+        rent_new_total = rent_new + new_travel_cost*4 + util + food
+        rent_858_new = rent_858 + current_util + travel_cost*4 + food
+        
+        monthly_savings_old = inflow - rent_858_new
+        monthly_savings_new = inflow - rent_new_total
+        text1 = f"Savings of Current House: {monthly_savings_old}  |  Savings of New House: {monthly_savings_new}"
+        st.write(text1)
         
     
     
